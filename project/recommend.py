9016fca5579610e06c5267dfafc6d8909a2f5cca
WazedKhan <wazedkhan119399@gmail.com>
2023-12-13 23:37:21 +0600

    post request

--- project/recommend.py
+++ project/recommend.py
@@ -1,11 +1,20 @@
 import pymongo
-
+import requests
 from icecream import ic
 
-client = pymongo.MongoClient("mongodb+srv://wazedkhan119399:7LRU8OS2BQv2N00U@cluster0.edtgmjj.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(
+    "mongodb+srv://wazedkhan119399:7LRU8OS2BQv2N00U@cluster0.edtgmjj.mongodb.net/?retryWrites=true&w=majority"
+)
 db = client.sample_mflix
 collection = db.movies
-results = collection.find().limit(5)
 
-for result in results:
-    ic(result)
+
+hugging_face_token = "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"
+embedding_url = "https://api-inference.huggingface.co/pipeline/feature-extraction/sentence-transformers/all-MiniLM-L6-v2"
+
+response = requests.post(
+    embedding_url,
+    headers={
+        "Authorization": f"Bearer {hugging_face_token}",
+    },
+)
